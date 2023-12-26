104126e1d02b9a87a563c887ee05cf85cf60abdc
Song <glen0125@foxmail.com>
2023-12-26 19:45:48 +0800

    Add order param

--- src/service/biz/inscribeService.js
+++ src/service/biz/inscribeService.js
@@ -14,83 +14,210 @@ class InscribeService {
         this.m_inited = true;
     }
 
-    async _getInscription(ctx) {
-        let hash = ctx.query.hash;
-        let address = ctx.query.address;
-        let offset = ctx.query.offset;
-        let length = ctx.query.length;
-
-        logger.debug('_getInscription:', hash, address);
-        if (hash) {
-            //find by hash
-            let ret = this.m_store.queryInscriptionByHash(hash);
-            return {
-                code: ret == null ? 1 : 0,
-                data: ret
-            };
-        } else if (address && offset && length) {
-            //find by owner
-            let { count, list } = this.m_store.queryInscriptionByAddress(address, offset, length);
-            return {
-                code: 0,
-                count: count,
-                list: list,
-            }
-        } else {
+    async _getInscriptionByHash(ctx) {
+        let hash = ctx.params.hash;
+        if (!hash) {
             return {
                 code: 1,
                 msg: "invalid param"
             };
         }
+
+        let ret = this.m_store.queryInscriptionByHash(hash);
+        return {
+            code: ret == null ? 1 : 0,
+            data: ret
+        };
     }
 
-    async _getInscriptionByHash(hash) {
+    async _getInscriptionByAddress(ctx) {
+        let address = ctx.params.address;
+        let offset = ctx.params.offset;
+        let length = ctx.params.length;
+        let order = ctx.params.order;
 
-    }
-
-    async _getResonanceList(ctx) {
-        let hash = ctx.query.hash;
-        let address = ctx.query.address;
-        if (hash) {
-            //find by hash
-        } else if (address) {
-            //find by address
-        } else {
+        if (!address) {
             return {
                 code: 1,
                 msg: "invalid param"
             };
         }
+
+        let { count, list } = this.m_store.queryInscriptionByAddress(
+            address,
+            length == 0 || length == null ? Number.MAX_SAFE_INTEGER : length,
+            offset || 0,
+            order ? order.toUpperCase() : "DESC"
+        );
+        return {
+            code: 0,
+            count: count,
+            list: list,
+        }
     }
 
-    async _getChantList(ctx) {
-        let hash = ctx.query.hash;
-        let address = ctx.query.address;
-        if (hash) {
-            //find by hash
-        } else if (address) {
-            //find by address
-        } else {
+    async _getInscriptionByBlock(ctx) {
+        let beginBlock = ctx.params.begin_block;
+        let endBlock = ctx.params.end_block;
+        let offset = ctx.params.offset;
+        let length = ctx.params.length;
+        let order = ctx.params.order;
+
+        if (!beginBlock) {
             return {
                 code: 1,
                 msg: "invalid param"
             };
+        }
+
+        let { count, list } = this.m_store.queryInscriptionByBlock(
+            beginBlock,
+            endBlock == 0 || endBlock == null ? Number.MAX_SAFE_INTEGER : endBlock,
+            length == 0 || length == null ? Number.MAX_SAFE_INTEGER : length,
+            offset || 0,
+            order ? order.toUpperCase() : "DESC"
+        );
+        return {
+            code: 0,
+            count: count,
+            list: list,
+        }
+    }
+
+    async _getResonanceByHash(ctx) {
+        let hash = ctx.params.hash;
+        let offset = ctx.params.offset;
+        let length = ctx.params.length;
+        let order = ctx.params.order;
+
+        if (!hash) {
+            return {
+                code: 1,
+                msg: "invalid param"
+            };
+        }
+
+        let { count, list } = this.m_store.queryResonanceByHash(
+            hash,
+            length == 0 || length == null ? Number.MAX_SAFE_INTEGER : length,
+            offset || 0,
+            order ? order.toUpperCase() : "DESC"
+        );
+        return {
+            code: 0,
+            count: count,
+            list: list,
+        }
+    }
+
+    async _getResonanceByAddress(ctx) {
+        let address = ctx.params.address;
+        let offset = ctx.params.offset;
+        let length = ctx.params.length;
+        let order = ctx.params.order;
+
+        if (!address) {
+            return {
+                code: 1,
+                msg: "invalid param"
+            };
+        }
+
+        let { count, list } = this.m_store.queryResonanceByAddress(
+            address,
+            length == 0 || length == null ? Number.MAX_SAFE_INTEGER : length,
+            offset || 0,
+            order ? order.toUpperCase() : "DESC"
+        );
+        return {
+            code: 0,
+            count: count,
+            list: list,
+        }
+    }
+
+    async _getChantByHash(ctx) {
+        let hash = ctx.params.hash;
+        let offset = ctx.params.offset;
+        let length = ctx.params.length;
+        let order = ctx.params.order;
+
+        if (!hash) {
+            return {
+                code: 1,
+                msg: "invalid param"
+            };
+        }
+
+        let { count, list } = this.m_store.queryChantByHash(
+            hash,
+            length == 0 || length == null ? Number.MAX_SAFE_INTEGER : length,
+            offset || 0,
+            order ? order.toUpperCase() : "DESC"
+        );
+        return {
+            code: 0,
+            count: count,
+            list: list,
+        }
+    }
+
+    async _getChantByAddress(ctx) {
+        let address = ctx.params.address;
+        let offset = ctx.params.offset;
+        let length = ctx.params.length;
+        let order = ctx.params.order;
+
+        if (!address) {
+            return {
+                code: 1,
+                msg: "invalid param"
+            };
+        }
+
+        let { count, list } = this.m_store.queryChantByAddress(
+            address,
+            length == 0 || length == null ? Number.MAX_SAFE_INTEGER : length,
+            offset || 0,
+            order ? order.toUpperCase() : "DESC"
+        );
+        return {
+            code: 0,
+            count: count,
+            list: list,
         }
     }
 
     registerRouter(router) {
         this._init();
 
-        router.get("/inscription", async (ctx) => {
-            ctx.response.body = await this._getInscription(ctx);
+        router.get("/inscription/:hash", async (ctx) => {
+            ctx.response.body = await this._getInscriptionByHash(ctx);
         });
 
-        router.get("/resonance_list", async (ctx) => {
-            ctx.response.body = await this._getResonanceList(ctx);
+        //order - asc or desc
+        router.get("/inscription_by_address/:address/:length?/:offset?/:order?", async (ctx) => {
+            ctx.response.body = await this._getInscriptionByAddress(ctx);
         });
 
-        router.get("/chant_list", async (ctx) => {
-            ctx.response.body = await this._getChantList(ctx);
+        router.get("/inscription_by_block/:begin_block/:end_block?/:length?/:offset?/:order?", async (ctx) => {
+            ctx.response.body = await this._getInscriptionByBlock(ctx);
+        });
+
+        router.get("/resonance_by_hash/:hash/:length?/:offset?/:order?", async (ctx) => {
+            ctx.response.body = await this._getResonanceByHash(ctx);
+        });
+
+        router.get("/resonance_by_address/:address/:length?/:offset?/:order?", async (ctx) => {
+            ctx.response.body = await this._getResonanceByAddress(ctx);
+        });
+
+        router.get("/chant_by_hash/:hash/:length?/:offset?/:order?", async (ctx) => {
+            ctx.response.body = await this._getChantByHash(ctx);
+        });
+
+        router.get("/chant_by_address/:address/:length?/:offset?/:order?", async (ctx) => {
+            ctx.response.body = await this._getChantByAddress(ctx);
         });
 
         return 0;
--- src/service/biz/inscribeStore.js
+++ src/service/biz/inscribeStore.js
@@ -1,78 +1,105 @@
-const Database = require('better-sqlite3');
-
-const DB_PATH = './data/inscribe.db';
+const { store } = require('./store');
 
 class InscribeStore {
     constructor() {
-        this.m_db = null;
-        // for test
-        /*this.m_db = new Database(DB_PATH);
-        this.m_db.exec(`CREATE TABLE IF NOT EXISTS inscribe (
-            hash VARCHAR(64) UNIQUE PRIMARY KEY NOT NULL,
-            address varchar(45) not null,
-            balance varchar(64) not null,
-            price varchar(64) not null,
-            block INT NOT NULL,
-            resonance_count INT NOT NULL DEFAULT 0
-        );`);
-
-        this.m_db.exec(`CREATE INDEX IF NOT EXISTS inscribe_address ON inscribe(address);`);
-        this.m_db.exec(`CREATE INDEX IF NOT EXISTS inscribe_block ON inscribe(block);`);
-        try {
-            let insertStmt = this.m_db.prepare(`INSERT INTO inscribe (hash, address, balance, price, block, resonance_count) VALUES (?, ?, ?, ?, ?, ?)`);
-            let insertResult = insertStmt.run('abc123', '0xaddr123', '0', '0', 555, 0);
-            insertStmt = this.m_db.prepare(`INSERT INTO inscribe (hash, address, balance, price, block, resonance_count) VALUES (?, ?, ?, ?, ?, ?)`);
-            insertResult = insertStmt.run('abc234', '0xaddr234', '10', '0', 560, 1);
-            insertStmt = this.m_db.prepare(`INSERT INTO inscribe (hash, address, balance, price, block, resonance_count) VALUES (?, ?, ?, ?, ?, ?)`);
-            insertResult = insertStmt.run('abc345', '0xaddr345', '100', '0', 570, 2);
-        } catch (error) {
-
-        }*/
-        // for test
-
-        this._initDB();
-    }
-
-    _initDB() {
-        if (this.m_db) {
-            return true;
-        }
-        try {
-            this.m_db = new Database(DB_PATH, { fileMustExist: true });
-            return true;
-        } catch (err) {
-            console.warn('open db failed:', err);
-        }
-
-        return false;
     }
 
     // return null or data
     queryInscriptionByHash(hash) {
-        if (!this._initDB()) {
-            return null;
-        }
-        const stmt = this.m_db.prepare('SELECT * FROM inscribe WHERE hash = ?');
+        const stmt = store.db.prepare('SELECT * FROM inscribe WHERE hash = ?');
         const ret = stmt.get(hash);
 
-        logger.debug('queryInscriptionByHash:', ret);
+        logger.debug('queryInscriptionByHash:', hash, "ret:", ret);
 
         return ret;
     }
 
     //return {count, list}
-    queryInscriptionByAddress(address, offset, length) {
-        if (!this._initDB()) {
-            return null;
-        }
-        const countStmt = this.m_db.prepare('SELECT COUNT(*) AS count FROM inscribe WHERE address = ?');
+    queryInscriptionByAddress(address, length, offset, order) {
+        order = order == "ASC" ? "ASC" : "DESC";
+        const countStmt = store.db.prepare('SELECT COUNT(*) AS count FROM inscribe WHERE address = ?');
         const countResult = countStmt.get(address);
         const count = countResult.count;
 
-        const pageStmt = this.m_db.prepare(`SELECT * FROM inscribe WHERE address = ? LIMIT ? OFFSET ?`);
+        const pageStmt = store.db.prepare(`SELECT * FROM inscribe WHERE address = ? ORDER BY block ${order} LIMIT ? OFFSET ?`);
         const list = pageStmt.all(address, length, offset);
 
-        logger.debug('queryInscriptionByAddress:', count, list);
+        logger.debug('queryInscriptionByAddress:', address, offset, length, "ret:", count, list);
+
+        return { count, list };
+    }
+
+    //[begin, end) return {count, list}
+    queryInscriptionByBlock(beginBlock, endBlock, length, offset, order) {
+        order = order == "ASC" ? "ASC" : "DESC";
+        const countStmt = store.db.prepare('SELECT COUNT(*) AS count FROM inscribe WHERE block >= ? AND block < ?');
+        const countResult = countStmt.get(beginBlock, endBlock);
+        const count = countResult.count;
+
+        const pageStmt = store.db.prepare(`SELECT * FROM inscribe WHERE block >= ? AND block < ? ORDER BY block ${order} LIMIT ? OFFSET ?`);
+        const list = pageStmt.all(beginBlock, endBlock, length, offset);
+
+        logger.debug('queryInscriptionByBlock:', beginBlock, endBlock, offset, length, "ret:", count, list);
+
+        return { count, list };
+    }
+
+    // return {count, list}
+    queryResonanceByHash(hash, length, offset, order) {
+        order = order == "ASC" ? "ASC" : "DESC";
+        const countStmt = store.db.prepare('SELECT COUNT(*) AS count FROM inscribe WHERE hash = ?');
+        const countResult = countStmt.get(hash);
+        const count = countResult.count;
+
+        const pageStmt = store.db.prepare(`SELECT * FROM inscribe WHERE hash = ? ORDER BY block ${order} LIMIT ? OFFSET ?`);
+        const list = pageStmt.all(hash, length, offset);
+
+        logger.debug('queryResonanceByHash:', hash, offset, length, "ret:", count, list);
+
+        return { count, list };
+    }
+
+    // return {count, list}
+    queryResonanceByAddress(address, length, offset, order) {
+        order = order == "ASC" ? "ASC" : "DESC";
+        const countStmt = store.db.prepare('SELECT COUNT(*) AS count FROM inscribe WHERE address = ?');
+        const countResult = countStmt.get(address);
+        const count = countResult.count;
+
+        const pageStmt = store.db.prepare(`SELECT * FROM inscribe WHERE address = ? ORDER BY block ${order} LIMIT ? OFFSET ?`);
+        const list = pageStmt.all(address, length, offset);
+
+        logger.debug('queryResonanceByAddress:', address, offset, length, "ret:", count, list);
+
+        return { count, list };
+    }
+
+    // return {count, list}
+    queryChantByHash(hash, length, offset, order) {
+        order = order == "ASC" ? "ASC" : "DESC";
+        const countStmt = store.db.prepare('SELECT COUNT(*) AS count FROM inscribe WHERE hash = ?');
+        const countResult = countStmt.get(hash);
+        const count = countResult.count;
+
+        const pageStmt = store.db.prepare(`SELECT * FROM inscribe WHERE hash = ? ORDER BY block ${order} LIMIT ? OFFSET ?`);
+        const list = pageStmt.all(hash, length, offset);
+
+        logger.debug('queryChantByHash:', hash, offset, length, "ret:", count, list);
+
+        return { count, list };
+    }
+
+    // return {count, list}
+    queryChantByAddress(address, length, offset, order) {
+        order = order == "ASC" ? "ASC" : "DESC";
+        const countStmt = store.db.prepare('SELECT COUNT(*) AS count FROM inscribe WHERE address = ?');
+        const countResult = countStmt.get(address);
+        const count = countResult.count;
+
+        const pageStmt = store.db.prepare(`SELECT * FROM inscribe WHERE address = ? ORDER BY block ${order} LIMIT ? OFFSET ?`);
+        const list = pageStmt.all(address, length, offset);
+
+        logger.debug('queryChantByAddress:', address, offset, length, "ret:", count, list);
 
         return { count, list };
     }
