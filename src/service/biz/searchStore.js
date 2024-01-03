b3a9eb691e2685b0e968350325718741296dc261
Song <glen0125@foxmail.com>
2024-01-03 16:33:09 +0800

    Return only success record

--- src/service/biz/searchStore.js
+++ src/service/biz/searchStore.js
@@ -12,8 +12,12 @@ class SearchStore {
         }
 
         try {
-            let stmt = store.indexDB.prepare(`SELECT * FROM ${TABLE_NAME.MINT_RECORDS} WHERE txid = ?`);
-            let ret = stmt.get(txid);
+            let stmt = store.indexDB.prepare(
+                `SELECT * 
+                FROM ${TABLE_NAME.MINT_RECORDS} 
+                WHERE txid = ? AND state = ?`
+            );
+            let ret = stmt.get(txid, InscriptionOpState.OK);
 
             logger.debug('queryByTxid:', txid, " find mint records, ret:", ret);
             if (ret) {
@@ -21,40 +25,60 @@ class SearchStore {
                 return makeSuccessReponse(ret);
             }
 
-            stmt = store.indexDB.prepare(`SELECT * FROM ${TABLE_NAME.INSCRIBE_RECORDS} WHERE txid = ?`);
-            ret = stmt.get(txid);
+            stmt = store.indexDB.prepare(
+                `SELECT * 
+                FROM ${TABLE_NAME.INSCRIBE_RECORDS} 
+                WHERE txid = ? AND state = ?`
+            );
+            ret = stmt.get(txid, InscriptionOpState.OK);
             logger.debug('queryByTxid:', txid, " find inscribe records, ret:", ret);
             if (ret) {
                 ret.type = "inscribe";
                 return makeSuccessReponse(ret);
             }
 
-            stmt = store.indexDB.prepare(`SELECT * FROM ${TABLE_NAME.RESONANCE_RECORDS} WHERE txid = ?`);
-            ret = stmt.get(txid);
+            stmt = store.indexDB.prepare(
+                `SELECT * 
+                FROM ${TABLE_NAME.RESONANCE_RECORDS} 
+                WHERE txid = ? AND state = ?`
+            );
+            ret = stmt.get(txid, InscriptionOpState.OK);
             logger.debug('queryByTxid:', txid, " find resonance records, ret:", ret);
             if (ret) {
                 ret.type = "resonance";
                 return makeSuccessReponse(ret);
             }
 
-            stmt = store.indexDB.prepare(`SELECT * FROM ${TABLE_NAME.CHANT_RECORDS} WHERE txid = ?`);
-            ret = stmt.get(txid);
+            stmt = store.indexDB.prepare(
+                `SELECT * 
+                FROM ${TABLE_NAME.CHANT_RECORDS} 
+                WHERE txid = ? AND state = ?`
+            );
+            ret = stmt.get(txid, InscriptionOpState.OK);
             logger.debug('queryByTxid:', txid, " find chant records, ret:", ret);
             if (ret) {
                 ret.type = "chant";
                 return makeSuccessReponse(ret);
             }
 
-            stmt = store.indexDB.prepare(`SELECT * FROM ${TABLE_NAME.TRANSFER_RECORDS} WHERE txid = ?`);
-            ret = stmt.get(txid);
+            stmt = store.indexDB.prepare(
+                `SELECT * 
+                FROM ${TABLE_NAME.TRANSFER_RECORDS} 
+                WHERE txid = ? AND state = ?`
+            );
+            ret = stmt.get(txid, InscriptionOpState.OK);
             logger.debug('queryByTxid:', txid, " find transfer records, ret:", ret);
             if (ret) {
                 ret.type = "transfer";
                 return makeSuccessReponse(ret);
             }
 
-            stmt = store.indexDB.prepare(`SELECT * FROM ${TABLE_NAME.SET_PRICE_RECORDS} WHERE txid = ?`);
-            ret = stmt.get(txid);
+            stmt = store.indexDB.prepare(
+                `SELECT * 
+                FROM ${TABLE_NAME.SET_PRICE_RECORDS} 
+                WHERE txid = ? AND state = ?`
+            );
+            ret = stmt.get(txid, InscriptionOpState.OK);
             logger.debug('queryByTxid:', txid, " find set price records, ret:", ret);
             if (ret) {
                 ret.type = "set_price";
