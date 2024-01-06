3a5fed1e613437b5a21b7a513b05f241d2859d88
X-MAN <lurenpluto@gmail.com>
2024-01-06 12:46:55 -0800

    Add reset command for clean state and data

--- src/main.js
+++ src/main.js
@@ -29,7 +29,13 @@ async function main() {
             describe: 'Mode of operation',
             choices: ['sync', 'index', 'both'],
         })
+        .option('reset', {
+            alias: 'r',
+            type: 'boolean',
+            describe: 'Reset state and all data, base on mode',
+        })
         .help().argv;
+
     const config_name = argv.config;
     console.log(`config name: ${config_name}`);
 
@@ -38,7 +44,80 @@ async function main() {
     assert(fs.existsSync(config_path), `config file not found: ${config_path}`);
     const config = new Config(config_path);
 
+    if (argv['reset']) {
+        reset(config.config, argv.mode);
+        return { ret: 0 };
+    }
+
     return await run_with_lock(config.config, argv.mode);
+}
+
+function reset(config, mode) {
+    assert(_.isString(mode), `mode should be string, but ${mode}`);
+
+    if (mode === 'sync' || mode === 'both') {
+        reset_sync(config);
+    }
+
+    if (mode === 'index' || mode === 'both') {
+        reset_index(config);
+    }
+}
+
+function reset_sync(config) {
+    const {SYNC_STATE_DB_FILE, ETH_INDEX_DB_FILE, INSCRIPTION_DB_FILE, TRANSFER_DB_FILE } = require('./constants');
+
+    // delete all the db files above
+
+    const { ret: get_dir_ret, dir } = Util.get_data_dir(config);
+    if (get_dir_ret !== 0) {
+        throw new Error(`failed to get data dir`);
+    }
+
+    const sync_state_db_file = path.join(dir, SYNC_STATE_DB_FILE);
+    if (fs.existsSync(sync_state_db_file)) {
+        console.warn(`remove sync state db: ${sync_state_db_file}`);
+        fs.unlinkSync(sync_state_db_file);
+    }
+
+    const eth_index_db_file = path.join(dir, ETH_INDEX_DB_FILE);
+    if (fs.existsSync(eth_index_db_file)) {
+        console.warn(`remove eth index db: ${eth_index_db_file}`);
+        fs.unlinkSync(eth_index_db_file);
+    }
+
+    const inscription_db_file = path.join(dir, INSCRIPTION_DB_FILE);
+    if (fs.existsSync(inscription_db_file)) {
+        console.warn(`remove inscription db: ${inscription_db_file}`);
+        fs.unlinkSync(inscription_db_file);
+    }
+
+    const transfer_db_file = path.join(dir, TRANSFER_DB_FILE);
+    if (fs.existsSync(transfer_db_file)) {
+        console.warn(`remove transfer db: ${transfer_db_file}`);
+        fs.unlinkSync(transfer_db_file);
+    }
+}
+
+function reset_index(config) {
+    const {INDEX_STATE_DB_FILE, TOKEN_INDEX_DB_FILE} = require('./constants');
+
+    const { ret: get_dir_ret, dir } = Util.get_data_dir(config);
+    if (get_dir_ret !== 0) {
+        throw new Error(`failed to get data dir`);
+    }
+
+    const index_state_db_file = path.join(dir, INDEX_STATE_DB_FILE);
+    if (fs.existsSync(index_state_db_file)) {
+        console.warn(`remove index state db: ${index_state_db_file}`);
+        fs.unlinkSync(index_state_db_file);
+    }
+
+    const token_index_db_file = path.join(dir, TOKEN_INDEX_DB_FILE);
+    if (fs.existsSync(token_index_db_file)) {
+        console.warn(`remove token index db: ${token_index_db_file}`);
+        fs.unlinkSync(token_index_db_file);
+    }
 }
 
 async function run(config, mode) {
