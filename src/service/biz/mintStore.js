02de082604690d69dda9a4ee4e343409cda2dad2
Song <glen0125@foxmail.com>
2024-01-10 11:24:08 +0800

    Add some debug log

--- src/service/biz/mintStore.js
+++ src/service/biz/mintStore.js
@@ -337,6 +337,7 @@ class MintStore {
             const response = await fetch(url);
 
             if (response.status != 200) {
+                log.info(response)
                 return makeResponse(ERR_CODE.UNKNOWN_ERROR, response.body);
             }
 
@@ -359,7 +360,7 @@ class MintStore {
 
             if (response.status != 200) {
                 log.info(response)
-                return makeResponse(ERR_CODE.UNKNOWN_ERROR, response);
+                return makeResponse(ERR_CODE.UNKNOWN_ERROR, response.body);
             }
 
             const json = await response.json();
