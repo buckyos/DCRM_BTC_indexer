623ece32ff04dc288a1681f1ee9f1eafcd057d96
X-MAN <lurenpluto@gmail.com>
2024-01-26 09:06:30 -0800

    Add inscription scanner test cases for content test

---
+++ src/test/block_generator.js
@@ -0,0 +1,52 @@
+class BlockGenerator {
+    constructor() {
+        if (new.target === BlockGenerator) {
+            throw new TypeError(
+                'Cannot construct BlockGenerator instances directly',
+            );
+        }
+    }
+
+    *generatorFunction() {
+        throw new Error('Must override method');
+    }
+
+    next() {
+        return this.generator.next();
+    }
+}
+
+class FixedBlockGenerator extends BlockGenerator {
+    constructor(blockHeights) {
+        super();
+        this.blockHeights = blockHeights;
+        this.generator = this.generatorFunction();
+    }
+
+    *generatorFunction() {
+        for (let height of this.blockHeights) {
+            yield height;
+        }
+    }
+}
+
+class RangeBlockGenerator extends BlockGenerator {
+    constructor(range) {
+        super();
+        this.range = range;
+        this.generator = this.generatorFunction();
+    }
+
+    *generatorFunction() {
+        for (let i = this.range[0]; i <= this.range[1]; i++) {
+            yield i;
+        }
+    }
+}
+
+
+module.exports = {
+    BlockGenerator,
+    FixedBlockGenerator,
+    RangeBlockGenerator,
+};
--- src/test/content_scanner.js
+++ src/test/content_scanner.js
@@ -1,33 +1,33 @@
-const { InscriptionTransferMonitor } = require('../index/monitor');
+const {
+    FixedBlockGenerator,
+    RangeBlockGenerator,
+} = require('./block_generator');
 const { Config } = require('../config');
 const { OrdClient } = require('../btc/ord');
 const assert = require('assert');
-const { LogHelper } = require('../log/log');
 const moment = require('moment');
-const { FixedBlockGenerator, RangeBlockGenerator } = require('./block_generator');
 
-class InscriptionTransferMonitorRunner {
+const target_content_types = [
+    'text/plain;charset=utf-8',
+    'text/plain',
+    'application/json',
+];
+
+class ContentScanner {
     constructor(config) {
-        this.monitor = new InscriptionTransferMonitor(config);
+        this.config = config;
         this.ord_client = new OrdClient(config.ord.rpc_url);
     }
 
     async init() {
-        const { ret } = await this.monitor.init();
-        if (ret !== 0) {
-            console.error(`failed to init monitor`);
-            return { ret };
-        }
-
-        console.log(`init monitor success`);
         return { ret: 0 };
     }
 
-    async run(block_generator) {
+    async scan(block_generator) {
         let block_height;
         while ((block_height = block_generator.next().value) != null) {
             // calc during time and log
-            console.origin.log(`process block ${block_height}`);
+            // console.log(`process block ${block_height}`);
 
             // first process inscriptions in block
             const { ret: process_ret } = await this._process_block_inscriptions(
@@ -38,20 +38,11 @@ class InscriptionTransferMonitorRunner {
                 return { ret: process_ret };
             }
 
-            // process all tx in block
-            const { ret: process_tx_ret } = await this.monitor.process_block(
-                block_height,
-            );
-            if (process_tx_ret !== 0) {
-                console.error(`failed to process block tx`);
-                return { ret: process_tx_ret };
-            }
-
             // calc during time and log
             const now = Date.now();
             const ts = moment(now).format('YYYY-MM-DD HH:mm:ss.SSS');
 
-            console.origin.log(`process block success ${block_height} ${ts}`);
+            // console.log(`process block success ${block_height} ${ts}`);
         }
 
         return { ret: 0 };
@@ -92,36 +83,77 @@ class InscriptionTransferMonitorRunner {
                 `invalid inscription genesis block height: ${inscription.genesis_height} !== ${block_height}`,
             );
 
-            // get creator address and satpoint
-            const {
-                ret: get_address_ret,
-                satpoint: creator_satpoint,
-                address: creator_address,
-                value: creator_value,
-            } = await this.monitor.calc_create_satpoint(inscription_id);
-            if (get_address_ret !== 0) {
-                console.error(
-                    `failed to get creator address for ${inscription_id}`,
-                );
-                return { ret: get_address_ret };
+            // check content type
+            if (!this._check_content_type(inscription.content_type)) {
+                // console.debug( `skip invalid content type ${inscription.content_type}`,);
+                continue;
             }
 
-            const { ret: add_ret } = await this.monitor.add_new_inscription(
-                inscription_id,
-                inscription.inscription_number,
-                block_height,
-                inscription.timestamp,
-                creator_address,
-                creator_satpoint,
-                creator_value,
-            );
-            if (add_ret !== 0) {
-                console.error(
-                    `failed to add new inscription creator record ${inscription_id} ${creator_satpoint.to_string()}, ${creator_address}`,
-                );
-                return { ret: add_ret };
+            // process inscription content
+            const { ret: process_ret } =
+                await this._process_inscription_content(inscription);
+            if (process_ret !== 0) {
+                console.error(`failed to process inscription content`);
+                return { ret: process_ret };
             }
         }
+
+        return { ret: 0 };
+    }
+
+    _check_content_type(content_type) {
+        assert(content_type != null, `content_type should not be null`);
+
+        if (content_type === null) {
+            // console.debug(`skip empty content type`);
+            return false;
+        }
+
+        if (content_type.indexOf('html') >= 0) {
+            return true;
+        }
+
+        if (content_type.indexOf('cbor') >= 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    async _process_inscription_content(inscription) {
+        assert(inscription != null, `inscription should not be null`);
+
+        const { ret, data: content } =
+            await this.ord_client.get_content_by_inscription(
+                inscription.inscription_id,
+            );
+        if (ret !== 0 || content == null || !_.isString(content)) {
+            console.error(
+                `failed to get inscription content ${inscription.id}`,
+            );
+            return { ret };
+        }
+
+        if (
+            content.indexOf('src=') >= 0 ||
+            content.indexOf('href=') >= 0 ||
+            content.indexOf('url(') >= 0 ||
+            content.indexOf('url (') >= 0
+        ) {
+            console.log(
+                `got target content with ref ${inscription.inscription_id}, ${inscription.content_type}`,
+            );
+        }
+
+        if (content.indexOf('<img') >= 0 ) {
+            console.log(
+                `got target content with img ${inscription.inscription_id}, ${inscription.content_type}`,
+            );
+        }
+
+        //console.log(
+        //    `got target content ${inscription.inscription_id}, ${inscription.content_type}`,
+        //);
 
         return { ret: 0 };
     }
@@ -137,26 +169,17 @@ async function test() {
     assert(fs.existsSync(config_path), `config file not found: ${config_path}`);
     const config = new Config(config_path);
 
-    // init log
-    const log = new LogHelper(config.config);
-    log.path_console();
-    log.enable_console_target(false);
-
-    const runner = new InscriptionTransferMonitorRunner(config.config);
+    const runner = new ContentScanner(config.config);
     const { ret: init_ret } = await runner.init();
     if (init_ret !== 0) {
         console.error(`failed to init monitor runner`);
         return { ret: init_ret };
     }
 
-    const block_generator = new FixedBlockGenerator([
-        780234, 790355, 790488, 790862, 823382, 823385,
-    ]);
-
-    const large_block_generator = new RangeBlockGenerator([780234, 823573]);
-    const { ret } = await runner.run(large_block_generator);
+    const large_block_generator = new RangeBlockGenerator([780234, 9999999]);
+    const { ret } = await runner.scan(large_block_generator);
     if (ret !== 0) {
-        console.error(`failed to run monitor`);
+        console.error(`failed to run content scanner`);
         return { ret };
     }
 
